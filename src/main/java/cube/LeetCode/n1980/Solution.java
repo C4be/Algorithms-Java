07af8e7c9789a2c8bbaa3b3fc9f953b517e48d79
C4be <dmitryc4be@gmail.com>
2025-02-20 14:18:17 +0300

    add fast solution

--- src/main/java/cube/LeetCode/n1980/Solution.java
+++ src/main/java/cube/LeetCode/n1980/Solution.java
@@ -44,4 +44,12 @@ class Solution { // TODO: add fast example
 
     }
 
+    public String fastSolution(String[] nums) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < nums.length; ++i) {
+            sb.append((nums[i].charAt(i) == '0') ? '1' : '0');
+        }
+        return sb.toString();
+    }
+
 }
